82fb6012a385434b991df3ef4153f1ced6788708
Phil <philwekullo@gmail.com>
2024-03-01 08:57:55 +0300

    complete

--- lib/person.py
+++ lib/person.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+from string import capwords
 APPROVED_JOBS = [
     "Admin",
     "Customer Service",
@@ -16,4 +16,16 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name = 'John', job = 'Mason'):
+        self.set_name(name)
+        self.set_job(job)
+    def set_name(self, name):    
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+             self.name = capwords(name)
+        else:
+            print('Name must be string between 1 and 25 characters.')    
+    def set_job(self, job):        
+        if job in APPROVED_JOBS:
+            self.job = job
+        else:
+            print('Job must be in list of approved jobs.')
